d322b607c4b9e94062c0d51984327a1bffb73f0c
jhs512 <jangka512@gmail.com>
2022-02-26 12:48:53 +0900

    게시물 리스트 API 구현

--- app/Http/Controllers/API/ArticleController.php
+++ app/Http/Controllers/API/ArticleController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use App\Models\Article;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\ArticleSaveRequest;
+use App\Http\Resources\ArticleCollection;
 
 class ArticleController extends Controller
 {
@@ -12,15 +15,24 @@ class ArticleController extends Controller
     {
         $this->authorizeResource(Article::class, 'article');
     }
-    
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $articles = Article::with('user')->with('logined_user_reaction_points')->orderBy('id', 'desc');
+
+        if ($request->input('search_keyword')) {
+            $articles = $articles->where('title', 'like', "%{$request->input('search_keyword')}%");
+            $articles = $articles->orWhere('body', 'like', "%{$request->input('search_keyword')}%");
+        }
+
+        $articles = $articles->paginate(3)->withQueryString();
+
+        return new ArticleCollection($articles);
     }
 
     /**
@@ -52,9 +64,31 @@ class ArticleController extends Controller
      * @param  \App\Models\Article  $article
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Article $article)
+    public function update(ArticleSaveRequest $request, Article $article)
     {
-        //
+        $validated = $request->validated();
+
+        $article->title = $validated['title'];
+        $article->body = $validated['body'];
+
+        if (isset($validated['img_1__remove'])) {
+            if ($article->img_1) {
+                Storage::disk('public')->delete($article->img_1);
+            }
+            $article->img_1 = null;
+        }
+
+        if ($request->hasFile('img_1')) {
+            if ($article->img_1) {
+                Storage::disk('public')->delete($article->img_1);
+            }
+
+            $article->img_1 = $request->file('img_1')->store('article/' . date('Y/m/d'), 'public');
+        }
+
+        $article->save();
+
+        response()->json(['success' => 'success'], 200);
     }
 
     /**
